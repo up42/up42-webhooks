d14ff34afb1009f18bd0ee136dca84b4d83c6043
António P. P. Almeida <antonio.almeida@up42.com>
2022-05-27 19:29:07 +0200

    * Added missing time import.

--- snippets/pipedream_up42_webhook_order_status_handler.py
+++ snippets/pipedream_up42_webhook_order_status_handler.py
@@ -14,6 +14,7 @@ import re
 import base64
 from hmac import digest, compare_digest
 from hashlib import sha256
+from time import time
 
 # Get the response headers.
 response_headers = steps["trigger"]["event"]["headers"]
